e67d7919a8c495dd1e74c0f28655b10279ff96d9
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-06-14 04:56:48 -0700

    Add project files.

---
+++ XafSaveAction.Module/BusinessObjects/Cliente.cs
@@ -0,0 +1,63 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafSaveAction.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Cliente : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Cliente(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        string nombreCompleto;
+        string apellido;
+        string nombre;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Nombre
+        {
+            get => nombre;
+            set => SetPropertyValue(nameof(Nombre), ref nombre, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Apellido
+        {
+            get => apellido;
+            set => SetPropertyValue(nameof(Apellido), ref apellido, value);
+        }
+        
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string NombreCompleto
+        {
+            get => nombreCompleto;
+            set => SetPropertyValue(nameof(NombreCompleto), ref nombreCompleto, value);
+        }
+    }
+}
---
+++ XafSaveAction.Module/Controllers/SavingActionController.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafSaveAction.Module.Controllers
+{
+    public class SavingActionController : ViewController
+    {
+        public SavingActionController() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            this.TargetViewType = ViewType.DetailView;
+            
+        }
+        ModificationsController modificactioController;
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            modificactioController = this.Frame.GetController<ModificationsController>();
+            if (modificactioController != null)
+            {
+                modificactioController.SaveAction.Executing += SaveAction_Executing;
+                modificactioController.SaveAction.Executed += SaveAction_Executed;
+            }
+            // Perform various tasks depending on the target View.
+        }
+
+        private void SaveAction_Executed(object sender, DevExpress.ExpressApp.Actions.ActionBaseEventArgs e)
+        {
+           //Esto va a pasar despues de que guardo
+        }
+
+        private void SaveAction_Executing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //esto va a pasar antes de guardar
+        }
+
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+            if (modificactioController != null)
+            {
+                modificactioController.SaveAction.Executing -= SaveAction_Executing;
+                modificactioController.SaveAction.Executed -= SaveAction_Executed;
+            }
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+}
